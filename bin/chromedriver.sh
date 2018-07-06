ff6c72d69d05f446bc9b9d3b9d7ee87d30c7c6da
JR_Severn <jr.severn@nuance.com>
2018-07-06 13:31:46 -0700

    test

--- bin/chromedriver.sh
+++ bin/chromedriver.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/bin/chromedriver --verbose --log-path=/var/log/chromedriver --no-sandbox $@
+/usr/bin/chromedriver --verbose --log-path=/var/log/chromedriver --window-size=1366,768 --no-sandbox $@
 
--- bin/run-tests-in-virtual-screen.sh
+++ bin/run-tests-in-virtual-screen.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-xvfb-run --server-args="-screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_COLOUR_DEPTH} -ac" robot --outputDir /opt/robotframework/reports ${ROBOT_OPTIONS} /opt/robotframework/tests
+xvfb-run --server-args="-screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_COLOUR_DEPTH} -ac" robot --outputDir /opt/robotframework/reports ${ROBOT_OPTIONS} ${TEST_CASE} 
+#/opt/robotframework/tests
